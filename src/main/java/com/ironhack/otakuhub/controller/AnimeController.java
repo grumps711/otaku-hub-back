8e4da377eb664f6c491cf76fc71c5a7e2f3a7232
andresagdt515 <83793367+andresagdt515@users.noreply.github.com>
2023-01-05 18:31:27 +0100

    first_anime_endpoint

--- src/main/java/com/ironhack/otakuhub/controller/AnimeController.java
+++ src/main/java/com/ironhack/otakuhub/controller/AnimeController.java
@@ -2,6 +2,7 @@ package com.ironhack.otakuhub.controller;
 
 import com.ironhack.otakuhub.model.Anime;
 import com.ironhack.otakuhub.proxy.AnimeProxy;
+import com.ironhack.otakuhub.service.AnimeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,13 +13,13 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/public/anime") //TODO eliminar public una vez que configuremos la security
 public class AnimeController {
-
+    private final AnimeService animeService;
     private final AnimeProxy animeProxy;
 
     @GetMapping ("/search")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public List<Anime> getAnimeByTitle(@RequestParam String keyw){
-        return animeProxy.getAnimeUrl(keyw);
+        return animeService.getAnimeByTitle(keyw);
     }
 
 
--- src/main/java/com/ironhack/otakuhub/service/AnimeService.java
+++ src/main/java/com/ironhack/otakuhub/service/AnimeService.java
@@ -1,7 +1,17 @@
 package com.ironhack.otakuhub.service;
 
+import com.ironhack.otakuhub.model.Anime;
+import com.ironhack.otakuhub.proxy.AnimeProxy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class AnimeService {
+    private final AnimeProxy animeProxy;
+    public List<Anime> getAnimeByTitle(String keyw) {
+        return animeProxy.getAnimeUrl(keyw);
+    }
 }
