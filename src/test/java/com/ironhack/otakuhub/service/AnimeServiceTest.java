4d12563576593777efb1f0bbb68555afff6af7e4
andresagdt515 <83793367+andresagdt515@users.noreply.github.com>
2023-01-13 18:22:40 +0100

    addPoints

---
+++ src/test/java/com/ironhack/otakuhub/service/AnimeServiceTest.java
@@ -0,0 +1,40 @@
+package com.ironhack.otakuhub.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AnimeServiceTest {
+
+
+    @Test
+    void getAnimesByTitle() {
+
+    }
+
+    @Test
+    void getAnimeDetails() {
+    }
+
+    @Test
+    void getPopularAnimes() {
+    }
+
+    @Test
+    void getAnimesByGenre() {
+    }
+
+    @Test
+    void getEpisodeStreamingUrl() {
+    }
+
+    @Test
+    void getAnimesByScene() {
+    }
+
+    @Test
+    void getRandomAnimeSceneImage() {
+    }
+}
